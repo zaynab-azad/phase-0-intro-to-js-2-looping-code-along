dee7465451baf046301e4e7addb1a3640298d777
Zaynab Azad Khdir <zaynabbbazad20@gmail.com>
2022-11-23 21:25:22 +0300

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // Code your solutions in this file
+function writeCards(Arr, event) {
+  const newArr = [];
+  for (let i = 0; i < Arr.length; i++) {
+    newArr[i] = `Thank you, ${Arr[i]}, for the wonderful ${event} gift!`;
+  }
+  return newArr;
+}
+
+function countDown(number) {
+  while (number >= 0) {
+    console.log(number);
+    number--;
+  }
+}
